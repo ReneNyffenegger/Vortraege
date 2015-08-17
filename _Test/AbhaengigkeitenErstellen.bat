1e4e4ce68b9e306c54d494e190309ab9bfd7e81e
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-08-17 15:44:35 +0200

    Going on with _Test

---
+++ _Test/AbhaengigkeitenErstellen.bat
@@ -0,0 +1,5 @@
+@rem
+@rem  https://github.com/ReneNyffenegger/scripts-and-utilities/blob/master/createImage.pl
+@rem
+@C:\lib\scripts\createImage.pl --width=500  --height=300 --green=50 --blue=200 --red=100
+@C:\lib\scripts\createImage.pl --width=1000 --height=600 --green=50 --blue=200 --red=100
